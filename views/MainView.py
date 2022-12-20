21534b6918352fea57ea04583f73eb4df54fdda5
GnitLores <11030459+GnitLores@users.noreply.github.com>
2022-12-20 11:13:22 +0100

    plot image with transparent rectangle

--- views/MainView.py
+++ views/MainView.py
@@ -1,6 +1,14 @@
 from PySide6.QtWidgets import QMainWindow
 from PySide6.QtCore import Slot
 from views.ui_MainView import Ui_MainWindow
+import pyqtgraph as pg
+from PIL import Image
+import cv2
+import os
+import numpy as np
+from PySide6 import QtGui, QtCore
+from PySide6.QtGui import QBrush, QColor, QPainter, QPen
+from PySide6.QtCore import Qt
 
 
 class MainView(QMainWindow):
@@ -28,6 +36,20 @@ class MainView(QMainWindow):
         # set a default value
         self._main_controller.change_amount(42)
 
+        # imWidget.setImage(np.array(im.getdata()).reshape(im.size[1], im.size[0]))
+        img = cv2.imread(os.path.join("C:\code\Image_Label_GUI", "2001022.jpg"))
+        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+        plotWidget: pg.PlotWidget = self._ui.plotWidget
+        item = pg.ImageItem(img)
+        plotWidget.addItem(item)
+        plotWidget.getPlotItem().hideAxis("left")
+        plotWidget.getPlotItem().hideAxis("bottom")
+        plotWidget.hideButtons()
+
+        rect_item = RectItem(QtCore.QRectF(100, 100, 100, 100))
+        plotWidget.addItem(rect_item)
+
     # @pyqtSlot(int)
     @Slot(int)
     def on_amount_changed(self, value):
@@ -42,3 +64,33 @@ class MainView(QMainWindow):
     @Slot(int)
     def on_enable_reset_changed(self, value):
         self._ui.pushButton_reset.setEnabled(value)
+
+
+class RectItem(pg.GraphicsObject):
+    def __init__(self, rect, parent=None):
+        super().__init__(parent)
+        self._rect = rect
+        self.picture = QtGui.QPicture()
+        self._generate_picture()
+
+    @property
+    def rect(self):
+        return self._rect
+
+    def _generate_picture(self):
+        painter = QtGui.QPainter(self.picture)
+        painter.setPen(pg.mkPen("r", width=2))
+        # painter.setBrush(pg.mkBrush("g"))
+        # painter.drawRect(self.rect)
+        transparency = 20  # 0-255
+        painter.setBrush(
+            QBrush(QColor(255, 0, 0, transparency), Qt.BrushStyle.SolidPattern)
+        )
+        painter.drawRect(self.rect)
+        painter.end()
+
+    def paint(self, painter, option, widget=None):
+        painter.drawPicture(0, 0, self.picture)
+
+    def boundingRect(self):
+        return QtCore.QRectF(self.picture.boundingRect())
--- views/ui_MainView.py
+++ views/ui_MainView.py
@@ -8,72 +8,55 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    QTime,
-    QUrl,
-    Qt,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import (
-    QApplication,
-    QLabel,
-    QMainWindow,
-    QPushButton,
-    QSizePolicy,
-    QSpinBox,
-    QVBoxLayout,
-    QWidget,
-)
-import resources_rc
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QMainWindow,
+    QPushButton, QSizePolicy, QSpinBox, QVBoxLayout,
+    QWidget)
 
+from pyqtgraph import PlotWidget
+import resources_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
-            MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(243, 265)
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(914, 1067)
         self.centralwidget = QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
+        self.centralwidget.setObjectName(u"centralwidget")
         self.vboxLayout = QVBoxLayout(self.centralwidget)
-        self.vboxLayout.setObjectName("vboxLayout")
+        self.vboxLayout.setObjectName(u"vboxLayout")
         self.spinBox_amount = QSpinBox(self.centralwidget)
-        self.spinBox_amount.setObjectName("spinBox_amount")
+        self.spinBox_amount.setObjectName(u"spinBox_amount")
 
         self.vboxLayout.addWidget(self.spinBox_amount)
 
-        self.label_even_odd = QLabel(self.centralwidget)
-        self.label_even_odd.setObjectName("label_even_odd")
+        self.testFrame = QFrame(self.centralwidget)
+        self.testFrame.setObjectName(u"testFrame")
+        self.testFrame.setFrameShape(QFrame.StyledPanel)
+        self.testFrame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout = QVBoxLayout(self.testFrame)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.plotWidget = PlotWidget(self.testFrame)
+        self.plotWidget.setObjectName(u"plotWidget")
 
-        self.vboxLayout.addWidget(self.label_even_odd)
+        self.verticalLayout.addWidget(self.plotWidget)
+
+        self.label_even_odd = QLabel(self.testFrame)
+        self.label_even_odd.setObjectName(u"label_even_odd")
+
+        self.verticalLayout.addWidget(self.label_even_odd)
+
+
+        self.vboxLayout.addWidget(self.testFrame)
 
         self.pushButton_reset = QPushButton(self.centralwidget)
-        self.pushButton_reset.setObjectName("pushButton_reset")
+        self.pushButton_reset.setObjectName(u"pushButton_reset")
         self.pushButton_reset.setEnabled(False)
 
         self.vboxLayout.addWidget(self.pushButton_reset)
@@ -83,13 +66,10 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
 
         QMetaObject.connectSlotsByName(MainWindow)
-
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        self.pushButton_reset.setText(
-            QCoreApplication.translate("MainWindow", "Reset", None)
-        )
+        self.pushButton_reset.setText(QCoreApplication.translate("MainWindow", u"Reset", None))
         pass
-
     # retranslateUi
+
